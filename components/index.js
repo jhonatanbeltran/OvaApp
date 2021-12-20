a8ab55cd3c0a22f051907c2730e6b6ed3400cf3f
jhonatanbeltran <jhonariston@gmail.com>
2021-12-20 05:41:48 -0500

    100

--- components/index.js
+++ components/index.js
@@ -7,6 +7,6 @@ export {default as Author} from './Author';
 export {default as CommentsForm} from './CommentsForm';
 export {default as Comments} from './Comments';
 export {default as PostDetail} from './PostDetail';
-export {default as loader} from './Loader';
+
 
 
